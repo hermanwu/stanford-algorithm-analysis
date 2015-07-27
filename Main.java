69ea4967ac665bc05e9a6a2c53d726712f972fcb
Herman Wu <herman.wrt@gmail.com>
2015-07-27 01:03:07 -0400

    implemented quicksort

--- Main.java
+++ Main.java
@@ -3,11 +3,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-        //Sort sort = new Sort(new int[]{2,5,6,3,4});
-
-        //sort.Quicksort();
+        //Quicksort implementation;
         FileHandler fh = new FileHandler("src/inputFiles/QuickSort.txt");
         int[] quickSortArray = fh.convertToArray();
         System.out.println(Arrays.toString(quickSortArray));
--- Sort.java
+++ Sort.java
@@ -5,21 +5,53 @@ import java.util.Arrays;
  */
 public class Sort {
     int[] unsortedArray;
-    int[] sortedArray;
+    int totalComparison;
 
     public Sort(int[] importArray){
         this.unsortedArray = importArray;
     }
 
     public int[] Quicksort(){
-        int[] returnArray = new int[]{3,6,1,4,5,2};
-        QuicksortPartition(returnArray, 0, returnArray.length - 1);
+        totalComparison = 0;
+        int[] returnArray = unsortedArray;
+        QuicksortPartitionWithStartPivot(returnArray, 0, returnArray.length - 1);
         return returnArray;
     }
 
-    public void QuicksortPartition(int[] inputArray, int leftIndex, int rightIndex){
-        System.out.println(Arrays.toString(inputArray));
+    public void QuicksortPartitionWithStartPivot(int[] inputArray, int leftIndex, int rightIndex){
+        // if it is not a single array element
         if(leftIndex < rightIndex) {
+            totalComparison = totalComparison + (rightIndex - leftIndex);
+            int i = leftIndex;
+            // setup the first item the pivot
+            int pivot = inputArray[leftIndex];
+            // i is used to track the first element that is bigger than pivot
+            i = i + 1;
+            // j is used to track new encountered element
+            int j = i;
+            // as long as j has not reached to the end of the array
+            while (j <= rightIndex) {
+                if (inputArray[j] < pivot) {
+                    // when encounter an element that need to be moved to left part
+                    // then swap this element with the first element that is biggere than pivot
+                    swap(inputArray, j, i);
+                    // i starts to track the next element that is bigger than pivot
+                    i = i + 1;
+                }
+                // move to next new element
+                j = j + 1;
+            }
+            // set pivot to the correct position
+            swap(inputArray, i - 1, leftIndex);
+            QuicksortPartitionWithStartPivot(inputArray, leftIndex, i - 2);
+            QuicksortPartitionWithStartPivot(inputArray, i, rightIndex);
+        }
+    }
+
+    public void QuicksortPartitionWithEndPivot(int[] inputArray, int leftIndex, int rightIndex) {
+        if(leftIndex < rightIndex) {
+            totalComparison = totalComparison + (rightIndex - leftIndex);
+            swap(inputArray, leftIndex, rightIndex);
             int i = leftIndex;
             int pivot = inputArray[leftIndex];
             i = i + 1;
@@ -32,8 +64,30 @@ public class Sort {
                 j = j + 1;
             }
             swap(inputArray, i - 1, leftIndex);
-            QuicksortPartition(inputArray, leftIndex, i - 2);
-            QuicksortPartition(inputArray, i, rightIndex);
+            QuicksortPartitionWithEndPivot(inputArray, leftIndex, i - 2);
+            QuicksortPartitionWithEndPivot(inputArray, i, rightIndex);
+        }
+    }
+
+    public void QuicksortPartitionWithMedianOfThreePivot(int[] inputArray, int leftIndex, int rightIndex){
+        if(leftIndex < rightIndex) {
+            totalComparison = totalComparison + (rightIndex - leftIndex);
+            int pivotIndex = getIndexOfMedianOfThreePivot(inputArray, leftIndex, rightIndex);
+            swap(inputArray, leftIndex, pivotIndex);
+            int i = leftIndex;
+            int pivot = inputArray[leftIndex];
+            i = i + 1;
+            int j = i;
+            while (j <= rightIndex) {
+                if (inputArray[j] < pivot) {
+                    swap(inputArray, j, i);
+                    i = i + 1;
+                }
+                j = j + 1;
+            }
+            swap(inputArray, i - 1, leftIndex);
+            QuicksortPartitionWithMedianOfThreePivot(inputArray, leftIndex, i - 2);
+            QuicksortPartitionWithMedianOfThreePivot(inputArray, i, rightIndex);
         }
     }
 
@@ -41,6 +95,23 @@ public class Sort {
         int temp = array[indexA];
         array[indexA] = array[indexB];
         array[indexB] = temp;
+    }
+
+    public int getIndexOfMedianOfThreePivot(int[] inputArray, int left, int right){
+        int leftEntry = inputArray[left];
+        int rightEntry = inputArray[right];
+        int medianEntry = inputArray[left + (right - left) / 2];
+        if((leftEntry > rightEntry && leftEntry < medianEntry)
+                || (leftEntry < rightEntry && leftEntry > medianEntry)){
+            return left;
+        }
+        else if((rightEntry > leftEntry && rightEntry < medianEntry)
+                || (rightEntry < leftEntry && rightEntry > medianEntry)){
+            return right;
+        }
+        else{
+            return left + (right - left) / 2;
+        }
     }
 
 }
